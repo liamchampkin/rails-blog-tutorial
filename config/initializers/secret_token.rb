a9838bd8db853857de61e0e0211bdce59a1a54c3
liamcreative <liam.smith@oup.com>
2014-02-27 16:52:51 +0000

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'ed11d3d6a52e6f5c24d0d4f60084bbed9cd8972c70d92667f29b7c9aafbd17acad6b2997247eee21fef58f32cfab823888cd81423e253774157da35eee147ffc'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+SimpleCms::Application.routes.draw do
+  resources :posts
+ 
+  root to: "welcome#index"
+end
