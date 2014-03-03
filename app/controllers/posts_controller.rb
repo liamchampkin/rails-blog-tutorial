98e46691cf10c163738eb121e98beae73efb71a5
liamcreative <liam.smith@oup.com>
2014-03-03 09:49:48 +0000

    some changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
 	def new
-
+	  @post = Post.new
 	end
-
+	 
 	def create
-		@post = Post.new(params[:post])
-		@post.save
-		redirect_to @post
+	  @post = Post.new(params[:post].permit(:title, :text))
+	 
+	  if @post.save
+	    redirect_to @post
+	  else
+	    render 'new'
+	  end
 	end
 	def show
 	  	@post = Post.find(params[:id])
